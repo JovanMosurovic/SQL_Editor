cc1883885f99f864e0ae3f1b7442315d22467415
JovanMosurovic <mosurovicjovan@gmail.com>
2024-04-22 22:36:37 +0200

    Adding menu functions.

---
+++ menu/Menu.h
@@ -0,0 +1,20 @@
+
+#ifndef ELEMENTAL_SQL_IMPLEMENTATION_MENU_H
+#define ELEMENTAL_SQL_IMPLEMENTATION_MENU_H
+
+
+class Menu {
+
+public:
+    static void importDatabaseMenu();
+    //todo second menu
+
+private:
+    static void cleanConsole();
+
+    static void finishProgram();
+
+};
+
+
+#endif //ELEMENTAL_SQL_IMPLEMENTATION_MENU_H
