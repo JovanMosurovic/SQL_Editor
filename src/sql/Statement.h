06f785820e8d8aa053430262a865d894e0162205
JovanMosurovic <mosurovicjovan@gmail.com>
2024-05-01 17:46:34 +0200

    Created Stetement class and SelectStatement.

--- src/sql/Statement.h
+++ src/sql/Statement.h
@@ -2,8 +2,19 @@
 #ifndef ELEMENTAL_SQL_IMPLEMENTATION_STATEMENT_H
 #define ELEMENTAL_SQL_IMPLEMENTATION_STATEMENT_H
 
+#include <string>
+#include "../database/Database.h"
 
 class Statement {
+protected:
+    string query;
+
+public:
+    explicit Statement(string query);
+    virtual ~Statement();
+
+    virtual bool parse() = 0;
+    virtual void execute(Database &db) = 0;
 
 };
 
